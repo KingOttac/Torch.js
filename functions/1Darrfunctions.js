b7398d85d83731c734e98406e767df1fe147430c
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-09-01 16:48:45 -0700

    i am stupid

--- functions/1Darrfunctions.js
+++ functions/1Darrfunctions.js
@@ -22,7 +22,6 @@ function softmax(ARR) {
 	let arrtoreturn = [];
 	for (g = 0; g < ARR.length; g++) {
 		exsum += pow(e,ARR[g]/smtemperature);
-		print(pow(e,ARR[g]/smtemperature))
 	}
 	for (g = 0; g < ARR.length; g++) {
 		arrtoreturn[g] = pow(e,ARR[g]/smtemperature)/exsum;
