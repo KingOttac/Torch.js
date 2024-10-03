af25989bde7f77c6063ecf1a3ecf9d06867b61f9
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-10-02 19:17:27 -0700

    Update singlevalfunctions.js

--- functions/singlevalfunctions.js
+++ functions/singlevalfunctions.js
@@ -61,35 +61,3 @@ function positioners(x) {
 	return sin(x/sx);
 	
 }
-
-function adamW(parr,p0,p1,p2,p3,pv) {
-
-	let cc = correctcheck[pv][p0][p1][p2][p3];
-	if (abs(cc[cc.length-2]-cc[cc.length-1]) >= convthresh) {
-		//move in from array
-		let mtin = mt[pv][p0][p1][p2][p3];
-		let vtin = vt[pv][p0][p1][p2][p3];
-		let tspin = tsp[pv][p0][p1][p2][p3];
-
-		//calculate vec adjust
-		tspin++;
-		let randomspot = rr(learningset,convertedlines.length);
-		let rereturn = runexample(randomspot);
-		let gtin = cc[cc.length-1]-rereturn;
-		mtin = b1*mtin + (1-b1)*gtin;//get first vec change
-		vtin = b2*vtin + (1-b2)*pow(gtin,2);//get second vec change
-		if (pv == 5) {
-			parr[p0][p1][p2] -= alpha*(mtin/(1-pow(b1,tspin)))/(sqrt(vtin/(1-pow(b2,tspin))) + epsilon);
-		}//bias changes
-		else {
-			parr[p0][p1][p2][p3] -= alpha*(mtin/(1-pow(b1,tspin)))/(sqrt(vtin/(1-pow(b2,tspin))) + epsilon);
-		}//everything else
-
-		//move back changed values
-		mt[pv][p0][p1][p2][p3] = mtin;
-		vt[pv][p0][p1][p2][p3] = vtin;
-		tsp[pv][p0][p1][p2][p3] = tspin;
-		correctcheck[pv][p0][p1][p2][p3][cc.length] = rereturn;
-	}//vector adjustment
-
-}//implements adamW optimizer
