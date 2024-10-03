b58e758941f69c5ed540788af87ec959d10db276
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-10-02 20:06:26 -0700

    Update adamW.js

--- src/AI/optimizers/adamW.js
+++ src/AI/optimizers/adamW.js
@@ -1,34 +1,9 @@
-function adamW(p0,p1,p2,p3,pv) {
-		
-	function dimen(assign,arr,pv,val) {
-		if (assign == true) {
-			if (dsties[pv] == 2) {
-				arr[p0][p1] = val;
-			}//encoder changes
-			else if (dsties[pv] == 3) {
-				arr[p0][p1][p2] = val;
-			}//bias changes
-			else if (dsties[pv] == 4) {
-				arr[p0][p1][p2][p3] = val;
-			}//key, query, valdown, valup, weights
-		}
-		else {
-			if (dsties[pv] == 2) {
-				return arr[p0][p1];
-			}//encoder changes
-			else if (dsties[pv] == 3) {
-				return arr[p0][p1][p2];
-			}//bias changes
-			else if (dsties[pv] == 4) {
-				return arr[p0][p1][p2][p3];
-			}//key, query, valdown, valup, weights
-		}
-	}//different dimensional arrays- assign: set to or return
+function adamW(pv,p0,p1,p2,p3,dst) {
 
 	//move in from array
-	let mtin = dimen(false,mt[pv],pv);
-	let vtin = dimen(false,vt[pv],pv);
-	let tspin = dimen(false,tsp[pv],pv);
+	let mtin = dimen(false,mt[pv],dst);
+	let vtin = dimen(false,vt[pv],dst);
+	let tspin = dimen(false,tsp[pv],dst);
 
 	//calculate vec adjust
 	tspin++;
@@ -39,9 +14,9 @@ function adamW(p0,p1,p2,p3,pv) {
 	vtin = vtin + pow(gtin,2);//get second vec change
 
 	//move back changed values
-	dimen(true,mt[pv],pv,mtin)
-	dimen(true,vt[pv],pv,vtin)
-	dimen(true,tsp[pv],pv,tspin)
+	dimen(true,mt[pv],dst,mtin)
+	dimen(true,vt[pv],dst,vtin)
+	dimen(true,tsp[pv],dst,tspin)
 	cc[randomspot-learningset] = rereturn;
 
 	//send a value to param
@@ -53,4 +28,4 @@ function adamW(p0,p1,p2,p3,pv) {
 		return rturn;
 	}
 
-}
+}//network consts
