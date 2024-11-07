9fd3938a47cb31c55ea81573598832be9b702a9a
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-11-07 09:04:34 -0700

    remove 2d operators

--- src/functions/multiDarrfunctions.js
+++ src/functions/multiDarrfunctions.js
@@ -44,54 +44,6 @@ function matrixmult(ARR1,ARR2) {
 	
 }//can also be used with ([vec1],transpose([vec2])) for dot product
 
-function add2d(ARR1,ARR2) {
-	
-	let ra = ARR1;
-	for (g = 0; g < ARR1.length; g++) {
-		for (g1 = 0; g1 < ARR1[0].length; g1++) {
-			ra[g][g1] += ARR2[g][g1];
-		}
-	}
-	return ra;
-	
-}//adds two same size arrays
-
-function sub2d(ARR1,ARR2) {
-	
-	let ra = ARR1;
-	for (g = 0; g < ARR1.length; g++) {
-		for (g1 = 0; g1 < ARR1[0].length; g1++) {
-			ra[g][g1] -= ARR2[g][g1];
-		}
-	}
-	return ra;
-	
-}//adds two same size arrays
-
-function mult2d(ARR1,ARR2) {
-
-	let ra = ARR1;
-	for (g = 0; g < ARR1.length; g++) {
-		for (g1 = 0; g1 < ARR1[0].length; g1++) {
-			ra[g][g1] *= ARR2[g][g1];
-		}
-	}
-	return ra;
-	
-}//adds two same size arrays
-
-function div2d(ARR1,ARR2) {
-	
-	let ra = ARR1;
-	for (g = 0; g < ARR1.length; g++) {
-		for (g1 = 0; g1 < ARR1[0].length; g1++) {
-			ra[g][g1] /= ARR2[g][g1];
-		}
-	}
-	return ra;
-	
-}//adds two same size arrays
-
 function concatenate(ARR) {
 	
 	let ra = [];
