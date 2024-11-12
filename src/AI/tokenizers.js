ec50dcfc117d9a19e970dc99bc008189f317b5fc
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-11-12 09:22:25 -0700

    Create tokenizers.js

---
+++ src/AI/tokenizers.js
@@ -0,0 +1,30 @@
+function tokenizer(lines,type) {
+	
+	for (a = 0; a < sampleset; a++) {
+		
+		let listarr = [];
+		if (type == "space") {
+			listarr = lines[a] + " \n";
+			listarr = split(listarr," ");
+		}
+		else if (type == "char") {
+			listarr = lines[a] + "\n";
+			listarr = split(listarr,"");
+		}
+		for (b = 0; b < listarr.length; b++) {
+			if (listarr[b] != "\n" && type == "space") {
+				listarr[b] += " ";
+			}
+			convertedlines[convertedlines.length] = listarr[b];
+			if (untoken(listarr[b]) == -1) {
+				tokens[tokens.length] = listarr[b];
+			}
+		}
+		
+	}
+	
+	for (a = 0; a < convertedlines.length; a++) {
+		convertedlines[a] = untoken(convertedlines[a])
+	}//convert everything into numbers
+	
+}//converts one data file (from preload) into tokens in the final array
