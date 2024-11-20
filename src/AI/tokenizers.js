11f5814e6c2dfd3526d713f8f73b3b6211e91147
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-11-19 23:52:09 -0700

    Update tokenizers.js

--- src/AI/tokenizers.js
+++ src/AI/tokenizers.js
@@ -1,5 +1,6 @@
 function tokenizer(lines,type) {
-	
+
+	let convertedlines = [];
 	for (a = 0; a < sampleset; a++) {
 		
 		let listarr = [];
@@ -22,9 +23,10 @@ function tokenizer(lines,type) {
 		}
 		
 	}
-	
+
 	for (a = 0; a < convertedlines.length; a++) {
 		convertedlines[a] = untoken(convertedlines[a])
 	}//convert everything into numbers
+	return convertedlines;
 	
 }//converts one data file (from preload) into tokens in the final array
