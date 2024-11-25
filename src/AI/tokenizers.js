32898e4a0d9e64a5099f9bdaaf4eeb5eefbb6436
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-11-25 07:54:05 -0700

    Update tokenizers.js

--- src/AI/tokenizers.js
+++ src/AI/tokenizers.js
@@ -5,17 +5,25 @@ function tokenizer(lines,type) {
 		
 		let listarr = [];
 		if (type == "space") {
-			listarr = lines[a] + " \n";
+			listarr = lines[a];
 			listarr = split(listarr," ");
+			let rlistarr = [];
+			for (b = 0; b < listarr.length; b++) {
+				rlistarr[rlistarr.length] = listarr[b];
+				if (b != listarr.length-1) {
+					rlistarr[rlistarr.length] = " ";
+				}
+				else {
+					rlistarr[rlistarr.length] = "\n";
+				}
+			}
+			listarr = rlistarr;
 		}
 		else if (type == "char") {
 			listarr = lines[a] + "\n";
 			listarr = split(listarr,"");
 		}
 		for (b = 0; b < listarr.length; b++) {
-			if (listarr[b] != "\n" && type == "space") {
-				listarr[b] += " ";
-			}
 			convertedlines[convertedlines.length] = listarr[b];
 			if (untoken(listarr[b]) == -1) {
 				tokens[tokens.length] = listarr[b];
