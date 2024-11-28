a9e6225b7d285c27e59984c24370b984cb38e339
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-11-28 03:27:27 -0700

    Update runfunctions.js

--- src/AI/runfunctions.js
+++ src/AI/runfunctions.js
@@ -115,14 +115,16 @@ function runlinear(input,qlayers,sorted,allweights,allbiases) {
 
 	let nsra = [];
 	nsra[0] = input;
+	let ra = input;
 	for (a = 0; a < qlayers; a++) {
-		nsra[a+1] = activate(linear(CA(nsra[a]),allweights[a],allbiases[a]));
+		nsra[a+1] = linear(CA(ra),allweights[a],allbiases[a]);
+		ra = activate(nsra[a+1]);
 	}
 	if (sorted == true) {
-		return [Bsort(CA(nsra[qlayers]),outputarr),nsra];
+		return [Bsort(CA(ra),outputarr),nsra];
 	}
 	else {
-		return [CA(nsra[qlayers]),nsra];
+		return [CA(ra),nsra];
 	}
 
 }//takes in parameters for layers (corr to weights), if sorted, input 
