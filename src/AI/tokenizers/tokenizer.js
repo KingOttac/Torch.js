6f0c5d4499b2023bdd628c428744aad0257656ab
KingOttac <155270221+KingOttac@users.noreply.github.com>
2025-02-16 17:43:41 -0700

    Update tokenizer.js

--- src/AI/tokenizers/tokenizer.js
+++ src/AI/tokenizers/tokenizer.js
@@ -42,27 +42,3 @@ function tokenizer(lines,type) {
 	return convertedlines;
 	
 }//converts one data file (from preload) into tokens in the final array
-
-function tokenizerchar(lines,type) {
-
-	tokens[0] = "\n";
-	let convertedlines = [];
-	for (a = 0; convertedlines.length < int(type)*(learningset+sampleset); a++) {
-		convertedlines = concatenate([convertedlines,split(lines[a] + "\n","")]);
-	}
-	convertedlines = convertedlines.slice(0,int(type)*(learningset+sampleset));
-	let newcl = [];
-	for (a = 0; a < convertedlines.length; a += int(type)) {
-		newcl[a/int(type)] = join(convertedlines.slice(a,a+int(type)),"");
-		if (untoken(newcl[a/int(type)]) == -1) {
-			tokens[tokens.length] = newcl[a/int(type)];
-		}
-	}
-	convertedlines = newcl;
-
-	for (a = 0; a < convertedlines.length; a++) {
-		convertedlines[a] = untoken(convertedlines[a]);
-	}//convert everything into numbers
-	return convertedlines;
-	
-}//converts one data file (from preload) into tokens in the final array
