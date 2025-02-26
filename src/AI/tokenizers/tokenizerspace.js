e7ba74a8fac2e3ef1c910efe8a2e74bae48ae74b
KingOttac <155270221+KingOttac@users.noreply.github.com>
2025-02-26 08:10:24 -0700

    scoping issues

--- src/AI/tokenizers/tokenizerspace.js
+++ src/AI/tokenizers/tokenizerspace.js
@@ -2,12 +2,12 @@ function tokenizerspace(lines) {
 
 	tokens[0] = "\n";
 	let convertedlines = [];
-	for (a = 0; convertedlines.length < learningset+sampleset && a < lines.length; a++) {
+	for (let a = 0; convertedlines.length < learningset+sampleset && a < lines.length; a++) {
 		
 		let listarr = [];
 		listarr = lines[a];
 		listarr = split(listarr," ");
-		for (b = 0; b < listarr.length-1; b++) {
+		for (let b = 0; b < listarr.length-1; b++) {
 			listarr[b] += " ";
 		}
 		if (listarr[0] != "") {
@@ -16,7 +16,7 @@ function tokenizerspace(lines) {
 		else {
 			listarr[0] = "\n";
 		}
-		for (b = 0; b < listarr.length && convertedlines.length < learningset+sampleset; b++) {
+		for (let b = 0; b < listarr.length && convertedlines.length < learningset+sampleset; b++) {
 			convertedlines[convertedlines.length] = listarr[b];
 			if (untoken(listarr[b]) == -1) {
 				tokens[tokens.length] = listarr[b];
@@ -25,7 +25,7 @@ function tokenizerspace(lines) {
 		
 	}
 
-	for (a = 0; a < convertedlines.length; a++) {
+	for (let a = 0; a < convertedlines.length; a++) {
 		convertedlines[a] = untoken(convertedlines[a]);
 	}//convert everything into numbers
 	
