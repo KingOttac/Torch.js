f02ba885e45fe1d431af4e2bfea3b9d684184599
KingOttac <155270221+KingOttac@users.noreply.github.com>
2025-02-26 08:09:17 -0700

    Update tokenizerchar.js

--- src/AI/tokenizers/tokenizerchar.js
+++ src/AI/tokenizers/tokenizerchar.js
@@ -2,12 +2,12 @@ function tokenizerchar(lines,type) {
 
 	tokens[0] = "\n";
 	let convertedlines = [];
-	for (a = 0; convertedlines.length < int(type)*(learningset+sampleset); a++) {
+	for (let a = 0; convertedlines.length < int(type)*(learningset+sampleset); a++) {
 		convertedlines = concatenate([convertedlines,split(lines[a] + "\n","")]);
 	}
 	convertedlines = convertedlines.slice(0,int(type)*(learningset+sampleset));
 	let newcl = [];
-	for (a = 0; a < convertedlines.length; a += int(type)) {
+	for (let a = 0; a < convertedlines.length; a += int(type)) {
 		newcl[a/int(type)] = join(convertedlines.slice(a,a+int(type)),"");
 		if (untoken(newcl[a/int(type)]) == -1) {
 			tokens[tokens.length] = newcl[a/int(type)];
@@ -15,7 +15,7 @@ function tokenizerchar(lines,type) {
 	}
 	convertedlines = newcl;
 
-	for (a = 0; a < convertedlines.length; a++) {
+	for (let a = 0; a < convertedlines.length; a++) {
 		convertedlines[a] = untoken(convertedlines[a]);
 	}//convert everything into numbers
 	return convertedlines;
